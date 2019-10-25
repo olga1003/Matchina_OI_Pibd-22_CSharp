5c71f6e2981933d16282c63123f0c4ef7ec733af
helga5622@gmail.com <helga5622@gmail.com>
2019-10-25 19:47:21 +0400

    Лабораторная работа 2

---
+++ WindowsFormsLocomotive/FormLocomotive.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsLocomotive
+{
+    public partial class FormLocomotive : Form
+    {
+        private ITransport locomotive;
+        public FormLocomotive()
+        {
+            InitializeComponent();
+        }
+
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureBoxLocomotive.Width, pictureBoxLocomotive.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            locomotive.DrawCar(gr);
+            pictureBoxLocomotive.Image = bmp;
+        }
+
+        private void ButtonCreate_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            locomotive = new LocoTrain(rnd.Next(100, 300), rnd.Next(1000, 2000), Color.Blue);
+            locomotive.SetPosition(rnd.Next(10, 100), rnd.Next(10, 100), pictureBoxLocomotive.Width,
+           pictureBoxLocomotive.Height);
+            Draw();
+        }
+        private void ButtonCreateLocoTrain_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            locomotive = new TrainLocomotive(rnd.Next(100, 300), rnd.Next(1000, 2000), Color.Blue,
+           Color.Black, true, true,true);
+            locomotive.SetPosition(rnd.Next(10, 100), rnd.Next(10, 100), pictureBoxLocomotive.Width,
+           pictureBoxLocomotive.Height);
+            Draw();
+        }
+        private void buttonMove_Click(object sender, EventArgs e)
+        {
+            string name = (sender as Button).Name;
+            switch (name)
+            {
+                case "buttonUp":
+                    locomotive.MoveTransport(Direction.Up);
+                    break;
+                case "buttonDown":
+                    locomotive.MoveTransport(Direction.Down);
+                    break;
+                case "buttonLeft":
+                    locomotive.MoveTransport(Direction.Left);
+                    break;
+                case "buttonRight":
+                    locomotive.MoveTransport(Direction.Right);
+                    break;
+            }
+            Draw();
+        }
+
+       
+    }
+}
---
+++ WindowsFormsLocomotive/TrainLocomotive.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+public enum Direction
+{
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+namespace WindowsFormsLocomotive
+{
+    class TrainLocomotive : LocoTrain
+    {
+        public Color DopColor { private set; get; }
+        public bool Steam { private set; get; }
+        public bool Coal { private set; get; }
+        public bool Pipe { private set; get; }
+        public TrainLocomotive(int maxSpeed, float weight, Color mainColor, Color dopColor,
+bool steam, bool coal, bool pipe) :
+ base(maxSpeed, weight, mainColor)
+        {
+            DopColor = dopColor;
+            Steam = steam;
+            Coal = coal;
+            Pipe = pipe;
+            Random rnd = new Random();
+        }
+        public override void DrawCar(Graphics g)
+        {
+            Pen pen = new Pen(Color.Black);
+            Brush dopBrush = new SolidBrush(DopColor);
+             if (Coal)
+             {
+
+                Brush coo = new SolidBrush(DopColor);
+                g.FillEllipse(coo, _startPosX + 23, _startPosY + 35, 10, 7);
+
+            }
+            if (Steam)
+            {
+                Brush st = new SolidBrush(Color.Gray);
+                g.FillEllipse(st, _startPosX + 50, _startPosY + 10, 35, 15);
+
+            }
+            if (Pipe)
+            {
+                Brush st = new SolidBrush(DopColor);
+                g.FillRectangle(st, _startPosX + 70, _startPosY + 25, 10, 15);
+
+            }
+            base.DrawCar(g);         
+        }
+    }
+}
