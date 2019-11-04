80b7374d511259acef818e756b6d065b15f49850
helga5622@gmail.com <helga5622@gmail.com>
2019-11-04 13:52:33 +0400

    Лабораторная работа №4

--- WindowsFormsLocomotive/WindowsFormsLocomotive/TrainLocomotive.cs
+++ WindowsFormsLocomotive/WindowsFormsLocomotive/TrainLocomotive.cs
@@ -4,13 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
-public enum Direction
-{
-    Up,
-    Down,
-    Left,
-    Right
-}
 
 namespace WindowsFormsLocomotive
 {
